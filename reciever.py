c404f10ccb1d5c2591552511bdc0d1e3a82236a6
JoseLaube <joselaubedev@gmail.com>
2026-03-24 10:58:47 -0300

    Add files via upload

--- reciever.py
+++ reciever.py
@@ -1,23 +1,26 @@
 import socket
 import struct
 
+grupo_ip = '224.1.1.1'
+porta = 8888
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-ttl = 1
-ttl_binario = struct.pack('b', ttl)
-grupo_ip = '224.0.0.2'
-mreq = struct.pack('4s4s', socket.inet_aton(grupo_ip), socket.inet_aton('0.0.0.0'))
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_binario)
-
-porta = 8888
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 sock.bind(('', porta))
 
-ttl = 1
-ttl_binario = struct.pack('b', ttl)
-##sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, ttl_binario)
+# Entrar no grupo multicast
+mreq = struct.pack('4s4s', socket.inet_aton(grupo_ip), socket.inet_aton('0.0.0.0'))
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-while True:
-    data, adr = sock.recvfrom(1024)
-    resp = data.decode('utf-8')
-    print(resp, adr)
+print("Receiver pronto, escutando...")
+
+try:
+    while True:
+        data, adr = sock.recvfrom(1024)
+        print(f"Mensagem de {adr}: {data.decode('utf-8')}")
+except KeyboardInterrupt:
+    print("\nEncerrando receiver...")
+finally:
+    sock.close()
