65f04e44dfb66c4a974842a040650b6101eb3b08
Monique Mendes <110635046+moniquemdes@users.noreply.github.com>
2026-04-07 17:45:59 -0300

    Update Navio.java

--- rmi_porto/Navio.java
+++ rmi_porto/Navio.java
@@ -1,5 +1,11 @@
-
-
 public class Navio {
-    
+    Integer id;
+    String descricao;
+    Double capacidade;
+
+    public Navio(Integer id, String descricao, Double capacidade) {
+        this.id = id;
+        this.descricao = descricao;
+        this.capacidade = capacidade;
+    }
 }
