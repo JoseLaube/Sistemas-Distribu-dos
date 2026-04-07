ce8e293716b001c3ca658ef237fe38d68750db5c
Monique Mendes <110635046+moniquemdes@users.noreply.github.com>
2026-04-07 17:46:16 -0300

    Update Carga.java

--- rmi_porto/Carga.java
+++ rmi_porto/Carga.java
@@ -1,5 +1,11 @@
-
-
 public class Carga {
-    
+    Integer id;
+    String descricao;
+    Integer volume;
+
+    public Carga(Integer id, String descricao, Integer volume) {
+        this.id = id;
+        this.descricao = descricao;
+        this.volume = volume;
+    }
 }
