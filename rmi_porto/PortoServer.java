aabef09906f7a0c33429d08b77df684db749a87b
Monique Mendes <110635046+moniquemdes@users.noreply.github.com>
2026-04-07 17:45:30 -0300

    Update PortoServer.java

--- rmi_porto/PortoServer.java
+++ rmi_porto/PortoServer.java
@@ -9,6 +9,9 @@ public class PortoServer implements IServico {
 
     private List<String> navios;
     private List<String> cargas;
+    //aqui eu acho q precisava ser Map pra guardar os objetos Navio e Carga
+    // private Map<Integer, Navio> navios;
+    // private Map<Integer, Carga> cargas;
 
     // Construtor do Servidor
     public PortoServer() {
@@ -39,42 +42,75 @@ public class PortoServer implements IServico {
 
 
     @Override
-    public double embarcar(String descricao) throws RemoteException {
-        return 0.0;
+    public Integer cadastrar_navio(String descricao_navio, Integer capacidade, Integer id_navio) throws RemoteException {
+        navios.put(id_navio, new Navio(id_navio, descricao_navio, Double.valueOf(capacidade)));
+        return id_navio; 
+    }
+
+    @Override
+    public void remover_navio(Integer id_navio) throws RemoteException {
+        navios.remove(id_navio); 
+    }
+
+    @Override
+    public Integer cadastrar_carga(String descricao, Integer volume, Integer id_carga) throws RemoteException {
+        cargas.put(id_carga, new Carga(id_carga, descricao, volume));
+        return id_carga;
+    }
+
+    @Override
+    public void remover_carga(Integer id) throws RemoteException {
+        cargas.remove(id);
+    }
+
+    @Override
+    public double embarcar_carga(String descricao, Integer id_carga, Integer id_navio, Double capacidade, String descricao_navio, Integer volume) throws RemoteException {
+        Navio navio = navios.get(id_navio);
+        Carga carga = cargas.get(id_carga);
+
+        if (navio == null) {
+            System.out.println("Navio com ID " + id_navio + " não encontrado.");
+            return -1.0; 
+        }
+
+        if (carga == null) {
+            System.out.println("Carga com ID " + id_carga + " não encontrada.");
+            return -1.0; 
+        }
+
+        if (navio.capacidade < volume) {
+            System.out.println("Capacidade insuficiente no navio.");
+            return -1.0;
+        }
+
+        System.out.println("Embarcando carga '" + carga.descricao + "' no navio '" + navio.descricao + "' (ID: " + id_navio + ")");
+
+        navio.capacidade -= volume;
+        // cargas.remove(id_carga);
+        return navio.capacidade;
+    }
+
+    @Override
+    public String relatorio_navio() throws RemoteException {
+        StringBuilder sb = new StringBuilder("--- Relatório de Navios ---\n");
+        for (Navio n : navios.values()) {
+            sb.append("ID: ").append(n.id).append(" | Descrição: ").append(n.descricao).append(" | Cap. Restante: ").append(n.capacidade).append("\n");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String relatorio_carga() throws RemoteException { 
+        StringBuilder sb = new StringBuilder("--- Relatório de Cargas ---\n");
+        for (Carga c : cargas.values()) {
+            sb.append("ID: ").append(c.id).append(" | Descrição: ").append(c.descricao).append(" | Volume: ").append(c.volume).append("\n");
+        }
+        return sb.toString();
     }
 
     @Override
     public String relatorio_embarque() throws RemoteException {
         return "Relatório vazio";
-    }
-
-    @Override
-    public Integer cadastrar_navio(String descricao, Integer capacidade) throws RemoteException {
-        return null; 
-    }
-
-    @Override
-    public void remover_navio(Integer id) throws RemoteException {
-    }
-
-    @Override
-    public String relatorio_navio() throws RemoteException {
-        System.out.println("Teste de Debug!");
-        return "Relatório de navios vazio";
-    }
-
-    @Override
-    public Integer cadastrar_carga(String descricao, Integer volume) throws RemoteException {
-        return null;
-    }
-
-    @Override
-    public void remover_carga(Integer id) throws RemoteException {
-    }
-
-    @Override
-    public String relatorio_carga() throws RemoteException {
-        return "Relatório de cargas vazio";
     }
 
 }
